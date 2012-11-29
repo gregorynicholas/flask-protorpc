22ac7766319e7a71d8d7c6eafc502b755c1406b5
Gregory Nicholas <faction.gregory@gmail.com>
2012-11-28 23:03:47 -0800

    added debug output..

--- flask_protorpc/proto.py
+++ flask_protorpc/proto.py
@@ -188,7 +188,8 @@ def parse_request_msg(message_type, payload=False):
     _value = request.args.to_dict()
   elif request.method == 'POST':
     if payload:
-      _value = loads('{"payload": ' + request.form.get('payload', ''))
+      _value = request.form.to_dict()
+      # _value = loads('{"payload": ' + request.form.get('payload', ''))
     # parse request values from form variables..
     elif 'application/x-www-form-urlencoded' in request.content_type:
       # logging.error('parsing request from x-www-form-urlencoded.')
